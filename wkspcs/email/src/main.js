5bcb6ef99a7cc3b7f8a9ead2b7ffdd0a880674d1
Kevin Kane <kkane@ncartmuseum.org>
2024-01-26 11:04:34 -0500

    add email workspace

---
+++ wkspcs/email/src/main.js
@@ -0,0 +1,72 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
+const path = require("node:path");
+
+// Enable live reload for all the files inside your project directory for Electron too
+require("electron-reload")(__dirname, {
+  // Note that the path to electron may vary according to the main file
+  // Here we go up several levels to find the electron package folder
+  electron: require(path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    "node_modules/electron"
+  )),
+});
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    // width: 800,
+    // height: 600,
+    fullscreen: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // No application (top bar) menu
+  Menu.setApplicationMenu(null);
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "/index.html"));
+
+  // Register standard "Control+Shift+I" combo to toggle the DevTools on selection window
+  globalShortcut.register("Control+Shift+I", () => {
+    mainWindow.webContents.toggleDevTools();
+  });
+
+  // Register standard "Control+R" combo to reload the page.
+  globalShortcut.register("CommandOrControl+R", () => {
+    mainWindow.webContents.reload();
+  });
+
+  // Register escape key to quit the app
+  globalShortcut.register("ESC", function () {
+    app.quit();
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
