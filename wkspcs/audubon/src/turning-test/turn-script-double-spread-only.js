2f1e339faeecfc8d33ddc6fd089834e5a1e096d0
Kevin Kane <kkane@ncartmuseum.org>
2024-03-01 16:12:19 -0500

    add function to find and display page corners overlay for turning controls UI, get first and last views, added code to resizeViewport for responsively changing flipbook size

--- wkspcs/audubon/src/turning-test/turn-script-double-spread-only.js
+++ wkspcs/audubon/src/turning-test/turn-script-double-spread-only.js
@@ -1,8 +1,11 @@
 // To do
-// write a function to get first and last views from pages
+// x - write a function to get first and last views from pages
+// x - get corner positions -> create overlay makers
 // dynamically populate pages?
-// responsive sizing
-// get corner positions -> create overlay makers
+// responsive sizing - started but still needs responsive zoom
+// try out swipeLeft swipeRight zoom events? not sure of intended purpose...just console.log() them
+//
+// code organizing!
 //
 // footer buttons
 // modal
@@ -56,12 +59,126 @@ $(book).turn({
 
       // $(this).turn('center');
 
-      if (page === 2 || page === 3) {
+      const currentView = $(book).turn("view");
+
+      // if turned to first view
+      if (compareArrays(currentView, getFirstView())) {
         $(this).turn("peel", "tr");
       }
     },
   },
 });
+
+// get first view and last view
+// defined for case of allowing spreads only
+function getFirstView() {
+  return [2, 3];
+}
+
+function getLastView() {
+  const pages = $(book).turn("pages");
+  const options = $(book).turn("options");
+
+  switch (options.display) {
+    case "double":
+      return [pages - 2, pages - 1];
+    case "single":
+      return [pages];
+    default:
+      return null;
+  }
+}
+
+// console.log(`firstView is ${getFirstView()}`);
+// console.log(`lastView is ${getLastView()}`);
+
+// get corners - not accurate until Turn.js has positioned book
+// does Turn.js give an initialized function ?
+function getCornerPositions() {
+  const boundingClientRect = book.getBoundingClientRect();
+
+  // console.log(book.style.left);
+  // console.log(book.style.top);
+
+  const corners = {
+    tl: { x: boundingClientRect.x, y: boundingClientRect.y },
+    tr: {
+      x: boundingClientRect.x + boundingClientRect.width,
+      y: boundingClientRect.y,
+    },
+    bl: {
+      x: boundingClientRect.x,
+      y: boundingClientRect.y + boundingClientRect.height,
+    },
+    br: {
+      x: boundingClientRect.x + boundingClientRect.width,
+      y: boundingClientRect.y + boundingClientRect.height,
+    },
+  };
+
+  // console.log(boundingClientRect);
+  // console.log(corners);
+  return corners;
+}
+
+const swipeTopLeft = document.getElementById("swipe-tl");
+const swipeTopRight = document.getElementById("swipe-tr");
+const swipeBottomLeft = document.getElementById("swipe-bl");
+const swipeBottomRight = document.getElementById("swipe-br");
+
+function positionOverlayCorners() {
+  const corners = getCornerPositions();
+
+  const width = swipeTopLeft.clientWidth;
+  const height = swipeTopLeft.clientHeight;
+  console.log(width);
+
+  swipeTopLeft.style.left = corners.tl.x - width / 2 + "px";
+  swipeTopLeft.style.top = corners.tl.y - height / 2 + "px";
+
+  swipeTopRight.style.left = corners.tr.x - width / 2 + "px";
+  swipeTopRight.style.top = corners.tr.y - height / 2 + "px";
+
+  swipeBottomLeft.style.left = corners.bl.x - width / 2 + "px";
+  swipeBottomLeft.style.top = corners.bl.y - height / 2 + "px";
+
+  swipeBottomRight.style.left = corners.br.x - width / 2 + "px";
+  swipeBottomRight.style.top = corners.br.y - height / 2 + "px";
+}
+
+// getCornerPositions();
+// setTimeout(getCornerPositions, 1000);
+setTimeout(positionOverlayCorners, 1000);
+
+const cornerControlsOverlay = document.getElementById(
+  "corner-controls-overlay"
+);
+const controlsToggleBtn = document.getElementById("controls-toggle-btn");
+let hidden = true;
+
+controlsToggleBtn.addEventListener("click", controlsToggleBtnOnClick);
+
+function controlsToggleBtnOnClick() {
+  if (hidden) {
+    fadeIn(cornerControlsOverlay);
+  } else {
+    fadeOut(cornerControlsOverlay);
+  }
+
+  hidden = !hidden;
+}
+
+function fadeIn(element) {
+  element.classList.remove("inactive");
+  element.classList.add("active");
+}
+
+function fadeOut(element) {
+  element.classList.remove("active");
+  element.classList.add("inactive");
+}
+
+// prev, next button listners
 
 $(prevBtn).on("click", prevBtnOnClick);
 
@@ -94,10 +211,10 @@ $(book).bind("turning", function (e, page, c) {
 // disable next/prev buttons on first and last views
 
 function setTurnControls(view) {
-  if (compareArrays(view, [2, 3])) prevBtn.disabled = true;
+  if (compareArrays(view, getFirstView())) prevBtn.disabled = true;
   else prevBtn.disabled = false;
 
-  if (compareArrays(view, [8, 9])) nextBtn.disabled = true;
+  if (compareArrays(view, getLastView())) nextBtn.disabled = true;
   else nextBtn.disabled = false;
 }
 
@@ -156,12 +273,37 @@ $(zoomViewport).zoom({
   },
 });
 
+//
+// Calculate the width and height of a square within another square
+// used within resizeViewport
+function calculateBound(d) {
+  var bound = { width: d.width, height: d.height };
+
+  if (bound.width > d.boundWidth || bound.height > d.boundHeight) {
+    var rel = bound.width / bound.height;
+
+    if (
+      d.boundWidth / rel > d.boundHeight &&
+      d.boundHeight * rel <= d.boundWidth
+    ) {
+      bound.width = Math.round(d.boundHeight * rel);
+      bound.height = d.boundHeight;
+    } else {
+      bound.width = d.boundWidth;
+      bound.height = Math.round(d.boundWidth / rel);
+    }
+  }
+
+  return bound;
+}
+//
+
 function resizeViewport() {
   var width = $(window).width(),
     height = $(window).height();
-  // options = $('.magazine').turn('options');
+  options = $(book).turn("options");
 
-  // $(book).removeClass("animated");
+  $(book).removeClass("animated");
 
   $(zoomViewport)
     .css({
@@ -170,9 +312,45 @@ function resizeViewport() {
     })
     .zoom("resize");
 
-  // $(book).addClass("animated");
+  if ($(book).turn("zoom") == 1) {
+    var bound = calculateBound({
+      width: options.width,
+      height: options.height,
+      boundWidth: Math.min(options.width, width),
+      boundHeight: Math.min(options.height, height),
+    });
+
+    if (bound.width % 2 !== 0) bound.width -= 1;
+
+    if (bound.width != $(book).width() || bound.height != $(book).height()) {
+      $(book).turn("size", bound.width, bound.height);
+
+      if ($(book).turn("page") == 1) $(book).turn("peel", "br");
+
+      $(".next-button").css({
+        height: bound.height,
+        backgroundPosition: "-38px " + (bound.height / 2 - 32 / 2) + "px",
+      });
+      $(".previous-button").css({
+        height: bound.height,
+        backgroundPosition: "-4px " + (bound.height / 2 - 32 / 2) + "px",
+      });
+    }
+
+    $(book).css({ top: -bound.height / 2, left: -bound.width / 2 });
+  }
+
+  $(book).addClass("animated");
 }
 resizeViewport();
+
+$(window)
+  .resize(function () {
+    resizeViewport();
+  })
+  .bind("orientationchange", function () {
+    resizeViewport();
+  });
 
 // Zoom functions ------------------------------------------------
 
