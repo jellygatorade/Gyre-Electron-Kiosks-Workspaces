0d1bb3d2d55dcec23a08b899edd81fa121438959
Kevin Kane <kkane@ncartmuseum.org>
2024-04-22 12:21:07 -0400

    update electron-forge config for perf-arts

--- wkspcs/perf-arts/forge.config.js
+++ wkspcs/perf-arts/forge.config.js
@@ -1,29 +1,43 @@
 module.exports = {
   packagerConfig: {
-    asar: true,
+    /**
+     * Electron packager options:
+     * https://electron.github.io/packager/main/interfaces/Options.html
+     *
+     * but you can not override the following options as they are set by Electron Forge internally.
+     * dir
+     * arch
+     * platform
+     * out
+     * electronVersion
+     *
+     * See
+     * https://www.electronforge.io/config/configuration
+     */
+    // asar: false,
+    asar: { unpack: "*" },
+    icon: "./src/common/assets/icons/ncma-icon", // no file extension required
+    overwrite: true,
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
   ],
+  hooks: {
+    packageAfterCopy: async (
+      config,
+      buildPath,
+      electronVersion,
+      platform,
+      arch
+    ) => {},
+  },
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
