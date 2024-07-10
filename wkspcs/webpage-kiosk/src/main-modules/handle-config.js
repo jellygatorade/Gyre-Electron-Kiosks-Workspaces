6404134629b0ef095dcfed5babb71397437d0b06
Kevin Kane <kkane@ncartmuseum.org>
2024-07-10 12:45:15 -0400

    add forceHardReset option to electron-reload

--- wkspcs/webpage-kiosk/src/main-modules/handle-config.js
+++ wkspcs/webpage-kiosk/src/main-modules/handle-config.js
@@ -29,8 +29,6 @@ function prepareProd() {
 
 function prepareDev() {
   console.log("initializing for development");
-  console.log(path.join(__dirname, ".."));
-  console.log(path.join(__dirname, "json-store "));
 
   // Enable live reload
   require("electron-reload")(
@@ -40,7 +38,7 @@ function prepareDev() {
     {
       // Provide path to electron package folder from this directory
       electron: require(path.join(__dirname, "..", "..", "..", "..", "node_modules/electron")),
-      // forceHardReset: true, // enables hard reset for every file change and not only the main file
+      forceHardReset: false, // enables hard reset for every file change and not only the main file
     }
   );
 }
