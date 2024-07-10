4f456c86338ab35bcc21dfeead5ffe3451635ab5
Kevin Kane <kkane@ncartmuseum.org>
2024-07-10 17:03:06 -0400

    remove unnecessary notes

--- wkspcs/webpage-kiosk/src/main-modules/json-store/config-store.js
+++ wkspcs/webpage-kiosk/src/main-modules/json-store/config-store.js
@@ -4,11 +4,6 @@ const Store = require("./store.js");
 
 // Store setup ------------------------------------------------
 
-// Old URLs
-// https://ncartmuseum.feedback/aib_vote_2024
-// http://gyre-web-tests.s3-website-us-east-1.amazonaws.com
-// https://nuxt-gyre-styles.pages.dev
-
 const defaults = {
   kiosk_webpage_url: "https://nuxt-gyre-styles.pages.dev",
   local_loading_page: path.join(__dirname, "..", "..", "/pages/loading/index.html"),
@@ -17,8 +12,7 @@ const defaults = {
 
 const configJSONStore = new Store.store({
   file_name: "config", // This string will get .json appended to it to become the filename
-  save_data: defaults, // Data will be saved underneath this key
-  // The data set here are defaults for the first instantiation
+  save_data: defaults, // Data will be saved underneath this key, defaults are for the first instantiation
   // This data will be appended to or replaced with existing data read by parseDataFile() in store.js
 });
 
@@ -27,14 +21,10 @@ const configJSONStore = new Store.store({
 ipcMain.on("update-app-config-store-data", function (event, formJSON) {
   // Form data
   configJSONStore.set("kiosk_webpage_url", formJSON?.kiosk_webpage_url);
+
   // Defaults
   configJSONStore.set("local_loading_page", defaults.local_loading_page);
   configJSONStore.set("local_config_page", defaults.local_config_page);
-
-  // Send reply back to sender
-  // To ensure is-reachable is using the new address
-  // event.reply("app-config-updated");
-  // THIS MAY NOT BE NEEDED
 });
 
 ipcMain.handle("get-app-config-store-data", async (event) => {
@@ -50,6 +40,3 @@ ipcMain.handle("get-app-config-form-defaults", async (event) => {
 
 // Expose the instance so that other main process modules may use its .get() or .set() methods
 module.exports.configJSONStore = configJSONStore;
-
-// Expose the configFormDefaults so that other main process modules may reset [.set()] the configJSONStore back to defaults
-// module.exports.configFormDefaults = configFormDefaults;
